f12e965d80f95e717976203af52d2b51850807fa
Diane Vail <dsval3@gmail.com>
2024-12-30 13:06:55 -0500

    webdev-js-intro-05

--- js/assignment.js
+++ js/assignment.js
@@ -8,11 +8,13 @@ const todos = [
     "Rake the leaves",
 ];
 
-// Declare your two immutable variables
 
+// immutable variables
+let shiftValue = "";
+let popValue = "";
 
 // Declaring constant variables to store references to the elements
-// that will be updated by your code.
+
 const answerOneEl = document.getElementById("answer-one");
 const answerTwoEl = document.getElementById("answer-two");
 const answerThreeEl = document.getElementById("answer-three");
@@ -21,47 +23,107 @@ const answerFiveEl = document.getElementById("answer-five");
 const answerSixEl = document.getElementById("answer-six");
 const submissionBtn = document.getElementById("submission-btn");
 
+// STEP 1 -- Checking in the console
+console.log(todos);
+//Output: (5) ['Wash the dishes', 'Clean the gutters', 'Mow the yard', 
+// 'Do some coding', 'Rake the leaves']
+
+//Output to assignment
 function updateAnswerOne() {
-    
+   answerOneEl.innerText = todos; 
+//Mow the yard,Clean the gutters,Wash the dishes,
+// Rake the leaves,Do some coding
 }
 
+//STEP 2 --  Checking in the console
+console.log(todos[2]); //Output: Mow the yard
+
+//Ouput to assignment page
 function updateAnswerTwo() {
-
+  answerTwoEl.innerText = todos[2];
+  //Output:  Wash the dishes
 }
 
-function removeLastValue() {
+//Step 3 
+popValue = todos.pop();
+//Checking in console
+console.log(popValue); //Output:  Rake the leaves
 
+function removeLastValue() {  
+  //This code only works outside of the function
+  //Do we really need this function??
 }
 
-function removeFirstValue() {
+//Step 4 -- checking in console
+console.log(todos); 
+//Output:  (4) ['Wash the dishes', 
+// 'Clean the gutters', 'Mow the yard', 'Do some coding']
 
-}
-
-function addShiftAndPopValues() {
-
-}
-
+//Ouput to assignment page
 function updateAnswerFour() {
+  answerFourEl.innerText = todos; 
+  //Output:  Mow the yard,Clean the gutters,
+  // Wash the dishes,Rake the leaves,Do some coding
+ } 
 
-}
+ //Step 5 
+const reversed = todos.reverse();
+
+//Checking in console
+  console.log(todos); 
+  //Output:  ['Do some coding', 'Mow the yard', 
+  // 'Clean the gutters', 'Wash the dishes'
 
 function reverseTodoList() {
-
+  //This code only works outside of the function
+  //Do we really need this function??
+  
 }
-
+//Step 6 -  Output to assignment
 function updateAnswerFive() {
+  answerFiveEl.innerText = todos;
+  //Ouptput:  Mow the yard,Clean the gutters,
+  // Wash the dishes,Rake the leaves,Do some coding.
+}
+//Step 7 -- CHecking in console
+shiftValue = todos.shift();
+console.log(shiftValue); //Output: Do some coding
 
+function removeFirstValue() {
+  //This code only works outside of the function
+  //Do we really need this function??
 }
 
+//Step 8 -- checking in console
+todos.push(popValue, shiftValue);
+console.log(todos);  //Output:  Mow the yard', 'Clean the gutters', 
+// 'Wash the dishes', 'Rake the leaves', 'Do some coding']
+
+function addShiftAndPopValues() {
+  //This code only works outside of the function
+  //Do we really need this function??
+}
+
+//Step 9
 function updateAnswerSix() {
-
+    answerSixEl.innerText = todos;
+// Ouput:  Mow the yard,Clean the gutters,
+// Wash the dishes,Rake the leaves,Do some coding
 }
+
 
 function render() {
-
+  updateAnswerOne();
+  updateAnswerTwo();
+  updateAnswerFour();
+  updateAnswerFive();
+  updateAnswerSix();
 }
-
 submissionBtn.addEventListener("click", function () {
-    // Update this function
-    render();
+  render();
 });
+//This code worked the first time through
+// but the submit button updates the answers at the end, so they are all wrong.
+//answers written to the console are correct, but do not match
+//the assignment page because we change the array before we push the answers
+
--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
-// Prevent us from attempting to use variables
+
+"use strict"// Prevent us from attempting to use variables
 // that are not declared
-"use strict"
+
+let supplyList =[10, "Tylenol", "Tampons", "IceCream", "Wine", "Tacos"]; //This list was my daughters idea :)
+console.log(supplyList); //Output [10, 'Tylenol', 'Tampons', 'IceCream', 'Wine', 'Tacos']
+typeof supplyList; //Output is (6) array
+
+//Use shift to remove the first item form an array
+supplyList.shift();
+console.log(supplyList); //removed 10 from arrray
+// new output is (5) ['Tylenol', 'Tampons', 'IceCream', 'Wine', 'Tacos']
+
+//Use push to add an item to the end of the array
+supplyList.push("Cookie Dough");
+console.log(supplyList);//output (6) ['Tylenol', 'Tampons', 'IceCream', 'Wine', 'Tacos', 'Cookie Dough'
+
+//Using the splice method to replace items in the array
+const start = 0;
+const deleteCount = 2;
+const removedItems = supplyList.splice(start, deleteCount, "Coffee", "Midol");
+console.log(supplyList);//(6) ['Coffee', 'Midol', 'IceCream', 'Wine', 'Tacos', 'Cookie Dough']
